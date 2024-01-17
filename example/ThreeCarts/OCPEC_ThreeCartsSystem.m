d505187ed78f97d00eb54852991287a2f4b8cc23
KY-Lin22 <linkangyu1994@gmail.com>
2024-01-17 17:16:45 +0900

    update example dynamics

--- example/ThreeCarts/OCPEC_ThreeCartsSystem.m
+++ example/ThreeCarts/OCPEC_ThreeCartsSystem.m
@@ -5,7 +5,7 @@ import casadi.*
 %%
 % time parameter
 TimeHorizon = 4; % time horizon T
-nStages = 100; % number of discretized stages
+nStages = 200; % number of discretized stages
 timeStep = TimeHorizon ./ nStages; % discretization time step
 
 % physical parameter
@@ -31,16 +31,16 @@ lambda = SX.sym('lambda', lambdaDim, 1);
 
 xMax = [8; 8; 8; 20; 20; 20];
 xMin = [-8; -8; -8; -20; -20; -20];
-uMax = 30;
-uMin = -30;
+uMax = 50;
+uMin = -50;
+lambdaMax = inf * ones(lambdaDim, 1); 
+lambdaMin = zeros(lambdaDim, 1);
 
 % cost function
 xWeight = [100; 20; 100; 1; 1; 1];
 uWeight = 1;
-lambdaWeight = [1; 1];
 L_S = 0.5 * (x - xRef)'*diag(xWeight)*(x - xRef)...
-    + 0.5 * u'*diag(uWeight)*u...
-    + 0.5 * lambda'*diag(lambdaWeight)*lambda;
+    + 0.5 * u'*diag(uWeight)*u;
 xWeight_T = [100; 20; 100; 100; 1; 100];
 L_T = 0.5 * (x - xRef)'*diag(xWeight_T)*(x - xRef);
 
@@ -51,12 +51,10 @@ f = [x(4);...
     - (c/m1) * x(4)              - (1/m1) * lambda(1);...
     - (c/m2) * x(5) + (1/m2) * u + (1/m2) * lambda(1) - (1/m2) * lambda(2);...
     - (c/m3) * x(6)                                   + (1/m3) * lambda(2)];% state equation f
-VISetType = 'nonnegative_orthant';
-bl = [0; 0];
-bu = [inf; inf]; 
 g = lambda; % VI set g >= 0
 F = [x(2) - x(1) - 0.5 * (L1 + L2);...
     x(3) - x(2) - 0.5 * (L2 + L3)]; % VI function F
+VISetType = 'nonnegative_orthant';
 
 % inequality constraint G >= 0
 G = [xMax - x;...
@@ -71,8 +69,9 @@ OCPEC = OCPEC_Formulation(...
     TimeHorizon, nStages, timeStep,...
     x0, xRef,...
     x, u, lambda,...
+    xMax, xMin, uMax, uMin, lambdaMax, lambdaMin,...
     L_T, L_S,...
-    f, g, F, VISetType, bl, bu,...
+    f, g, F, VISetType,...
     G, C);
 end
 

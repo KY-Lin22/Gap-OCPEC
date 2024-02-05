b8b026b991b4c676d7cd683a7a470adb792b0449
KY-Lin22 <linkangyu1994@gmail.com>
2024-02-05 13:17:02 +0900

    Update NLP_Formulation.m

--- src/@NLP_Formulation/NLP_Formulation.m
+++ src/@NLP_Formulation/NLP_Formulation.m
@@ -32,8 +32,8 @@ classdef NLP_Formulation < handle
             'exponential'...
             })} = 'quadratic'  % strongly convex function in Auchmuty's saddle function       
         gap_func_smooth_param double {mustBeNonnegative} = 0.001 % used in CHKS smoothing function for max(0, x) and mid(bl, bu, x)
-        D_gap_param_a double {mustBeNonnegative} = 0.9; % D gap function parameters: b > a > 0 (a ref value: a = 0.9, b = 1.1)
-        D_gap_param_b double {mustBeNonnegative} = 1.1; % Ref: Theoretical and numerical investigation of the D-gap function   
+        D_gap_param_a double {mustBeNonnegative} = 0.8; % D gap function parameters: b > a > 0 (a ref value: a = 0.9, b = 1.1)
+        D_gap_param_b double {mustBeNonnegative} = 1.2; % Ref: Theoretical and numerical investigation of the D-gap function   
                                                         % for BVI, 1998, Mathematical Programming, C.Kanzow & M. Fukushima                  
         penalty_gap_func_auxiliary_variable char {mustBeMember(penalty_gap_func_auxiliary_variable, {...
             'none',...

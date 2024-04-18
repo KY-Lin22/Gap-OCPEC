b50ecfa393b667c849ad2d2575677c038873b5ea
KY-Lin22 <linkangyu1994@gmail.com>
2024-04-18 12:59:38 +0900

    update performance profile test

--- performance_profile/test_D_gap_reformulation/test.m
+++ performance_profile/test_D_gap_reformulation/test.m
@@ -2,7 +2,7 @@ close all
 clear all
 clc
 
-% run test (need about 3 hours)
+% run test (need about 2 hours)
 % [Rec, NLP_reformulation_name] = run_test_D_gap_reformulation();
 
 save('Data_performance_test.mat', 'Rec', 'NLP_reformulation_name')
@@ -18,5 +18,5 @@ solver_name = Data_performance_test.NLP_reformulation_name;
 n_p = size(performance_matrix, 1);
 % problem_ploted_index = reshape([1 : 4: 44; 2 : 4: 44], [], 1);
 % solver_ploted_index = [1, 2, 3, 4, 5, 6, 7, 8];
-solver_ploted_index = 1 : 16;
+solver_ploted_index = [4, 8, 12, 16];
 plot_performance_profile(performance_matrix(:, solver_ploted_index), solver_name(solver_ploted_index))
--- performance_profile/test_primal_gap_reformulation/test.m
+++ performance_profile/test_primal_gap_reformulation/test.m
@@ -4,7 +4,7 @@ clear all
 clc
 
 % run test (it needs about 30 min)
-[Rec, NLP_reformulation_name] = run_test_primal_gap_reformulation();
+% [Rec, NLP_reformulation_name] = run_test_primal_gap_reformulation();
 
 save('Data_performance_test.mat', 'Rec', 'NLP_reformulation_name')
 
@@ -18,6 +18,6 @@ solver_name = Data_performance_test.NLP_reformulation_name;
 %
 n_p = size(performance_matrix, 1);
 % problem_ploted_index = reshape([1 : 4: 44; 2 : 4: 44], [], 1);
-solver_ploted_index = [1, 2, 3, 4, 5, 6];
+solver_ploted_index = [2, 3, 4, 6, 7,8];
 
 plot_performance_profile(performance_matrix(:, solver_ploted_index), solver_name(solver_ploted_index))

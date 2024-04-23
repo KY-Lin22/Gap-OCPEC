90e398a931400343a46f88006fb185f57878c26d
KY-Lin22 <linkangyu1994@gmail.com>
2024-04-23 13:09:16 +0900

    update example

--- example/AffineDVI/test_AffineDVI.m
+++ example/AffineDVI/test_AffineDVI.m
@@ -9,29 +9,28 @@ OCPEC = OCPEC_AffineDVI();
 Option.relaxation_problem = 'gap_constraint_based'; % 'gap_constraint_based', 'KKT_based'
 Option.gap_constraint_relaxation_strategy = 'generalized_primal_gap'; % 'generalized_primal_gap', 'generalized_D_gap'
 Option.KKT_complementarity_relaxation_strategy = 'Scholtes'; % 'Scholtes', 'Lin_Fukushima', 'Kadrani'
-Option.strongly_convex_func = 'quadratic'; % 'quadratic', 'exponential'
-Option.gap_func_smooth_param = 0.001;
-% Option.D_gap_param_a = 0.8;
-% Option.D_gap_param_b = 1.2;
-Option.gap_func_auxiliary_variable_penalty = 'L2'; % 'none', 'L1', 'L2'
+Option.strongly_convex_func = 'quadratic';
+Option.gap_func_smooth_param = 1e-4;
+Option.D_gap_param_a = 0.8;
+Option.D_gap_param_b = 1.2;
+Option.auxiliary_variable_strategy = 'none'; % 'none', 'omega'
 NLP = NLP_Formulation(OCPEC, Option);
 
 % create solver
 solver = IPOPT_Based_Solver(OCPEC, NLP);
 
 % create initial guess
-% z_Init = solver.create_initial_guess();
-z_Init = ones(NLP.Dim.z, 1);
+z_Init = randn(NLP.Dim.z, 1);
+
 %% parameter and problem solve
 % parameter
-s_Init = 1e1;
+s_Init = 1e-1;
 s_End = 1e-8; 
-mu_Init = 1e0;
-mu_End = 1e5;
-p_Init = [s_Init; mu_Init];
-p_End = [s_End; mu_End];
+p_Init = s_Init;
+p_End = s_End;
 solver.Option.Homotopy.kappa_s_times = 0.8;
 solver.Option.Homotopy.kappa_s_exp = 1.2;
+solver.Option.Homotopy.VI_nat_res_tol = 1e-2;
 
 % solve
 [z_Opt, Info] = solver.solve_NLP(z_Init, p_Init, p_End);
--- example/Stewart_simple_differential_inclusion/test_Stewart_simple_differential_inclusion.m
+++ example/Stewart_simple_differential_inclusion/test_Stewart_simple_differential_inclusion.m
@@ -7,32 +7,30 @@ OCPEC = OCPEC_Stewart_simple_differential_inclusion();
 
 % discretize OCPEC into a NLP problem
 Option.relaxation_problem = 'gap_constraint_based'; % 'gap_constraint_based', 'KKT_based'
-Option.gap_constraint_relaxation_strategy = 'generalized_primal_gap'; % 'generalized_primal_gap', 'generalized_D_gap'
+Option.gap_constraint_relaxation_strategy = 'generalized_D_gap'; % 'generalized_primal_gap', 'generalized_D_gap'
 Option.KKT_complementarity_relaxation_strategy = 'Scholtes'; % 'Scholtes', 'Lin_Fukushima', 'Kadrani'
 Option.strongly_convex_func = 'quadratic';
-Option.gap_func_smooth_param = 0.001;
+Option.gap_func_smooth_param = 1e-4;
 Option.D_gap_param_a = 0.8;
 Option.D_gap_param_b = 1.2;
-Option.gap_func_auxiliary_variable_penalty = 'L2'; % 'none', 'L1', 'L2'
+Option.auxiliary_variable_strategy = 'none'; % 'none', 'omega'
 NLP = NLP_Formulation(OCPEC, Option);
 
 % create solver
 solver = IPOPT_Based_Solver(OCPEC, NLP);
 
 % create initial guess
-% z_Init = solver.create_initial_guess();
-z_Init = zeros(NLP.Dim.z, 1);
+z_Init = randn(NLP.Dim.z, 1);
 
 %% parameter and problem solve
 % parameter
 s_Init = 1e-1;
-s_End = 1e-5; 
-mu_Init = 1e1;
-mu_End = 1e5;
-p_Init = [s_Init; mu_Init];
-p_End = [s_End; mu_End];
+s_End = 1e-8; 
+p_Init = s_Init;
+p_End = s_End;
 solver.Option.Homotopy.kappa_s_times = 0.8;
 solver.Option.Homotopy.kappa_s_exp = 1.2;
+solver.Option.Homotopy.VI_nat_res_tol = 1e-2;
 
 % solve
 [z_Opt, Info] = solver.solve_NLP(z_Init, p_Init, p_End);
